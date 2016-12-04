ab4ee2951134a30b5a5fe7d240a95c796ec6d158
jdhe1120 <jhe1120@gmail.com>
2016-12-04 00:28:43 -0500

    fixed syntax of submit screen and created box around submit buton

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -10,6 +10,7 @@ import
   TextInput,
   AsyncStorage,
   TouchableOpacity,
+  Dimensions
 } from 'react-native';
 import
 {
@@ -25,7 +26,7 @@ export default class LinksScreen extends React.Component
   constructor(props)
   {
     super(props);
-    this.state = {text: 'Enter table number:'};
+    this.state = {text: 'Enter table number'};
   }
 
   /* Submits input on press */
@@ -33,10 +34,15 @@ export default class LinksScreen extends React.Component
   {
     try
     {
-      const nameinfo = await AsyncStorage.getItem('sessionid');
-      if (nameinfo !== null)
+      // get users name and fb id from the data that was stored before globally
+      const userfbdata = await AsyncStorage.getItem('sessionid');
+
+      // if the userdata is there, set userId to the fb id and usersName to the user's name
+      if (userfbdata !== null)
       {
-        var jsonnameinfo = JSON.parse(nameinfo);
+        var objectuserdata = JSON.parse(userfbdata);
+        var userId = objectuserdata.id;
+        var usersName = objectuserdata.name;
       }
     }
     catch (error)
@@ -44,13 +50,12 @@ export default class LinksScreen extends React.Component
       console.log("error getting data from AsyncStorage");
     }
 
+    // change the submitted number to upperacse
     var submitTableNum = this.state.text.toUpperCase();
-
-    var userId = jsonnameinfo.id;
 
     let length = submitTableNum.length;
 
-    { /* Form validation */}
+    // do form validation checking ensuring the table is A B C and <= 17
     if (length < 2 || length > 3)
     {
       Alert.alert('Error', 'Please insert a valid table number.');
@@ -67,12 +72,11 @@ export default class LinksScreen extends React.Component
       {
         var tableDigits = parseInt(submitTableNum.substring(1));
 
-      {/* Enters a valid table number into Firebase */}
         if (tableDigits <= 17)
         {
-
+            // enter into database with user as key and tablenum and time as values
             firebase.database().ref('users/' + userId).set({
-            name: jsonnameinfo.name,
+            name: usersName,
             tablenumber: submitTableNum,
             time: new Date().getTime(),
             });
@@ -86,6 +90,7 @@ export default class LinksScreen extends React.Component
       }
     }
   }
+
   static route =
   {
     navigationBar:
@@ -96,29 +101,31 @@ export default class LinksScreen extends React.Component
 
   render()
   {
+    var width = Dimensions.get('window').width;
+    var height = Dimensions.get('window').height;
     return (
-      <View>
-      <View><TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        onChangeText={(text) => this.setState({text})}
-        value={this.state.text}
-        selectTextOnFocus={true}
-      />
-      <Button
-        style={{fontSize: 20, color: 'black'}}
-        styleDisabled={{color: 'red'}}
-        onPress={() => this._handlePress()}>
-        Submit
-      </Button>
-
-      <Text>
-      </Text>
-
-
+      <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        <View style={{flex: 0.4}}></View>
+        <View style={{flex: 0.2, backgroundColor: 'gainsboro', paddingHorizontal: 20, alignItems: 'center', justifyContent: 'center'}}>
+            <Text></Text>
+            <View style={{}}>
+              <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 2, width: width*0.8, textAlign:'center', borderRadius: 10}}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+                selectTextOnFocus={true}
+              />
+            </View>
+            <Text></Text>
+            <Button
+              style={{fontSize: 20, color: 'black'}}
+              styleDisabled={{color: 'red'}}
+              onPress={() => this._handlePress()}>
+              Submit
+            </Button>
+        </View>
+        <View style={{flex: 0.4}}><Text></Text></View>
       </View>
-
-      </View>
-
     );
   }
 }
